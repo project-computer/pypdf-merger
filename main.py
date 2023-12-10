10f392af3e55c92bea686f6378a85e97ae0d203a
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-12-11 01:02:43 +0700

    add feature to handle split by number

--- main.py
+++ main.py
@@ -2,7 +2,32 @@ from PyPDF2 import PdfReader, PdfWriter
 import os
 import glob
 import random
-def merge_pdfs_with_random_two_pages(pdf_directory, output_path):
+
+from select_algorithm import get_manual, get_split
+
+# def read_files(pdf_directory):
+#   """
+#   Reads all PDF files in a directory.
+
+#   Args:
+#     pdf_directory: Path to the directory containing PDF files.
+
+#   Returns:
+#     A list of PdfReader objects.
+#   """
+#   # Get all PDF paths in the directory
+#   pdf_paths = glob.glob(os.path.join(pdf_directory, "*.pdf"))
+
+#   # Process each PDF
+#   path_pages = {}
+#   for pdf_path in pdf_paths:
+#     reader = PdfReader(pdf_path)
+#     num_pages = len(reader.pages)
+#     path_pages[pdf_path] = num_pages
+
+#   return path_pages
+
+def merge_pdfs(pdf_directory, output_path, select_algorithm='r', per_file=2, step=2, start=0):
   """
   Merges multiple PDFs, adding two random pages from each file in a directory, into one PDF.
 
@@ -10,19 +35,27 @@ def merge_pdfs_with_random_two_pages(pdf_directory, output_path):
     pdf_directory: Path to the directory containing PDF files.
     output_path: Path to the merged PDF file.
   """
+ 
 
   writer = PdfWriter()
 
   # Get all PDF paths in the directory
   pdf_paths = glob.glob(os.path.join(pdf_directory, "*.pdf"))
-  print(pdf_paths)
   # Process each PDF
   for pdf_path in pdf_paths:
     reader = PdfReader(pdf_path)
     num_pages = len(reader.pages)
 
     # Randomly select two pages
-    selected_pages = random.sample(range(num_pages), 2)
+    
+    if (select_algorithm == 'r'):
+      selected_pages = random.sample(range(num_pages), per_file)
+    elif (select_algorithm == 's'):
+      selected_pages = get_split(num_pages, start, step, per_file)
+    elif (select_algorithm == 'm'):
+      selected_pages = get_manual(pdf_path,num_pages)
+
+    # selected_pages = random.sample(range(num_pages), 2)
     print(pdf_path,selected_pages)
     for page_index in selected_pages:
       page = reader.pages[page_index]
@@ -32,12 +65,25 @@ def merge_pdfs_with_random_two_pages(pdf_directory, output_path):
   with open(output_path, 'wb') as output_file:
     writer.write(output_file)
 
-# Example usage
-pdf_questions = "questions"
-pdf_solutions = "solutions"
-question_output_path = "merged_random.pdf"
-solution_output_path = "merged_random_solutions.pdf"
-merge_pdfs_with_random_two_pages(pdf_questions, question_output_path)
-merge_pdfs_with_random_two_pages(pdf_solutions, solution_output_path)
+if __name__ == "__main__":
+  select_algorithm = input("Select pages by (r)andom or (s)plit:  ")
+  per_file = 2
+  step=0
+  start=0
+  if (select_algorithm == 'r' or select_algorithm == 's'):
+    per_file = int(input("Enter number of pages to grab per file: "))
+    if (select_algorithm == 's'):
+      start = int(input("Enter start page: "))
+      step = int(input("Enter step size: "))
+  # # Example usage
+  pdf_questions = "questions"
+  pdf_solutions = "solutions"
+  question_output_path = "merged_random.pdf"
+  solution_output_path = "merged_random_solutions.pdf"
+  
+  # print(read_files(pdf_questions))
+  # print(read_files(pdf_solutions))
+  merge_pdfs(pdf_questions, question_output_path, select_algorithm, per_file, step, start)
+  merge_pdfs(pdf_solutions, solution_output_path, select_algorithm, per_file, step, start)
 
-print(f'Merged PDFs into {question_output_path} and {solution_output_path}')
+  print(f'Merged PDFs into {question_output_path} and {solution_output_path}')
---
+++ select_algorithm.py
@@ -0,0 +1,60 @@
+# Feature manual
+def get_manual(file_name,num_pages):
+  """
+  Gets user input for selecting pages from a PDF.
+
+  Args:
+    num_pages: The total number of pages in the PDF.
+
+  Returns:
+    A list of valid page numbers selected by the user.
+  """
+  pages = []
+  while True:
+    # Prompt user for input
+    user_input = input(f"for {file_name} Enter page numbers separated by spaces (0-{num_pages-1}): ")
+
+    # Check if input is empty
+    if not user_input:
+      break
+
+    # Split input into individual numbers
+    try:
+      page_numbers = [int(x) for x in user_input.split()]
+    except ValueError:
+      print("Invalid input: Please enter only integers.")
+      continue
+
+    # Validate page numbers
+    for page_number in page_numbers:
+      if page_number < 0 or page_number >= num_pages:
+        print(f"Invalid page number: {page_number}. Please enter a number between 0 and {num_pages-1}.")
+        continue
+      pages.append(page_number)
+
+    # Check if user wants to add more pages
+    add_more = input("Add more pages (y/N): ")
+    if add_more.lower() != "y":
+      break
+
+  return pages
+
+# Feature ทุก start step เช่นเว้นว่าง 3 4 5
+
+def get_split(num_pages, start, step, max_pages_per_split=10):
+  """
+  Generates a list of page numbers by splitting the document into chunks.
+
+  Args:
+    num_pages: The total number of pages in the PDF.
+    step: The step size for splitting the page range.
+    max_pages_per_split: The maximum number of pages allowed in each chunk.
+
+  Returns:
+    A list of page numbers for the first chunk of the document.
+  """
+  # Create a list of page numbers with the specified step size
+  page_list = list(range(start, num_pages, step))
+
+  # Limit the number of pages returned to the maximum per split
+  return page_list[:max_pages_per_split]
